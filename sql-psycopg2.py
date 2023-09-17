939869585349767dc0b45830c9eff048bc09a376
MickKav <>
2023-09-17 13:48:16 +0000

    Finish sql-orm

---
+++ sql-psycopg2.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - selectonly the name column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only 'queen' from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only bthe albums with "ArtistId" #51 on the "Artist" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
