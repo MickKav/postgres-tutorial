ff329447fc876180773d62eaf02703a345a2db0f
MickKav <>
2023-09-17 15:36:06 +0000

    CR of CRUD in lesson

--- sql-orm.py
+++ sql-orm.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# executin the instructions from the "chinook" database
+# executing the instructions from the "chinook" database
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
